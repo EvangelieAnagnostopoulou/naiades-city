7c1d4bfb04dd77e5a138fae505ebdb726d56de88
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2023-11-28 23:15:25 +0200

    Disable strict manifest

--- project/settings/production.py
+++ project/settings/production.py
@@ -2,4 +2,5 @@ from project.settings.staging import *
 
 DEBUG = os.environ.get("DEBUG_UNSAFE") == "ON"
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
